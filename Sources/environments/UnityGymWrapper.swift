2c812084c0b36ad7f6a13ac4e4ec48c08418a049
Yeshwanth <morpheyesh@gmail.com>
2020-09-22 23:43:37 +0100

    moar fixes

--- Sources/environments/UnityGymWrapper.swift
+++ Sources/environments/UnityGymWrapper.swift
@@ -8,7 +8,6 @@
 
 import Foundation
 
-class UnityGymException{}
 
 class UnityToGymWrapper {
 
@@ -18,6 +17,8 @@ class UnityToGymWrapper {
     var allowMultipleObs: Bool = false
     var previousDecisionStep: DecisionSteps
     var terminalStep: TerminalSteps
+    
+    var actionSpace:
     
     //Hidden flag used by Atari environments to determine if the game is over
     var gameOver: Bool
@@ -36,36 +37,8 @@ class UnityToGymWrapper {
         }
         
         let name = Array(behaviourSpec.keys)[0]
-        
-        //self.groupSpec = self.env.behaviorSpecs[self.name]
-        
-//        if self.getNVisObs() == 0 && self.getVecObsSize() == 0 {
-//            throw UnityGymException("""
-//                There are no observations provided by the environment
-//            """)
-//        }
-//
-//        if !(self.getNVisObs() >= 1 && uint8Visual) {
-//            logger.warning(
-//                "uint8_visual was set to true, but visual observations are not in use. "
-//                "This setting will not have any effect."
-//            )
-//        }
-//        else {
-//            self.uint8_visual = uint8Visual
-//        }
-//        if (
-//            self.getNVisObs() + self.getVecObsSize() >= 2
-//            && !self.allowMultipleObs
-//        ) {
-//            logger.warning(
-//                "The environment contains multiple observations. "
-//                "You must define allow_multiple_obs=True to receive them all. "
-//                "Otherwise, only the first visual observation (or vector observation if"
-//                "there are no visual observations) will be provided in the observation."
-//            )
-//        }
-        
+        let groupSpec = behaviourSpec[name]
+
         // Check for number of agents in scene.
         unityEnv.reset()
         let steps = unityEnv.getSteps(behaviorName: name)
@@ -75,47 +48,56 @@ class UnityToGymWrapper {
         self.previousDecisionStep = steps.0
 
         // Set action spaces
-        if self.groupSpec.isActionDiscrete() {
-            branches = self.groupSpec.discreteActionBranches
-            if self.groupSpec.actionShape == 1:
-                self._action_space = spaces.Discrete(branches[0])
-            else:
-                if flatten_branched:
-                    self._flattener = ActionFlattener(branches)
-                    self._action_space = self._flattener.action_space
-                else:
-                    self._action_space = spaces.MultiDiscrete(branches)
-
-        } else {
-            if flattenBranched {
-                logger.warning(
-                    "The environment has a non-discrete action space. It will "
-                    "not be flattened."
-                )
-            }
-            high = np.array([1] * self.group_spec.action_shape)
-            self.actionSpace = spaces.Box(-high, high, dtype=np.float32)
-        }
+//        if groupSpec!.isActionDiscrete {
+//            let branches = groupSpec!.discreteActionBranches
+//            if groupSpec!.actionShape == 1 {
+//                self._action_space = spaces.Discrete(branches[0])
+//            }
+//            else {
+//                if flatten_branched {
+//                    self._flattener = ActionFlattener(branches)
+//                    self._action_space = self._flattener.action_space
+//                } else {
+//                    self._action_space = spaces.MultiDiscrete(branches)
+//                }
+//            }
+//
+//        } else {
+//            if flattenBranched {
+//                logger.warning(
+//                    "The environment has a non-discrete action space. It will "
+//                    "not be flattened."
+//                )
+//            }
+//            high = np.array([1] * self.group_spec.action_shape)
+//            self.actionSpace = spaces.Box(-high, high, dtype=np.float32)
+//        }
         // Set observations space
-        list_spaces: List[gym.Space] = []
-        shapes = self.getVisObsShape()
-        for shape in shapes{
-            if uint8Visual:
-                list_spaces.append(spaces.Box(0, 255, dtype=np.uint8, shape=shape))
-            else:
-                list_spaces.append(spaces.Box(0, 1, dtype=np.float32, shape=shape))
-        }
-        if self.getVecObsSize() > 0 {
-            // vector observation is last
-            high = np.array([np.inf] * self._get_vec_obs_size())
-            list_spaces.append(spaces.Box(-high, high, dtype=np.float32))
-        }
-        if self.allowMultipleObs {
-            self.observationSpace = spaces.Tuple(list_spaces)
-        } else {
-            self.observationSpace = listSpaces[0]
-        }
+//        list_spaces: List[gym.Space] = []
+//        shapes = self.getVisObsShape()
+//        for shape in shapes{
+//            if uint8Visual:
+//                list_spaces.append(spaces.Box(0, 255, dtype=np.uint8, shape=shape))
+//            else:
+//                list_spaces.append(spaces.Box(0, 1, dtype=np.float32, shape=shape))
+//        }
+//        if self.getVecObsSize() > 0 {
+//            // vector observation is last
+//            high = np.array([np.inf] * self._get_vec_obs_size())
+//            list_spaces.append(spaces.Box(-high, high, dtype=np.float32))
+//        }
+//        if self.allowMultipleObs {
+//            self.observationSpace = spaces.Tuple(list_spaces)
+//        } else {
+//            self.observationSpace = listSpaces[0]
+//        }
     }
+    
+    
+    func reset(){}
+    func step(){}
+    func singleStep(){}
+    func close()
     
     
     func _check_agents(nAgents: Int) throws {
